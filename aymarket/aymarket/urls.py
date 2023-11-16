058bf905ad5100cae2204c0702ad31c584a24e4c
Kamil Ayupov <ayupovkamil@gmail.com>
2023-11-16 10:51:17 +0300

    Added detail page

--- aymarket/aymarket/urls.py
+++ aymarket/aymarket/urls.py
@@ -1,12 +1,13 @@
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
 
-from aymarket.core.views import index, contact
+from core.views import index, contact
 
 urlpatterns = [
-    path('contact/', contact, name='contact'),
     path('', index, name='index'),
+    path('contact/', contact, name='contact'),
+    path('items/', include('item.urls')),
     path('admin/', admin.site.urls),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- aymarket/core/views.py
+++ aymarket/core/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render
 
-from aymarket.item.models import Category, Item
+from item.models import Category, Item
 
 
 def index(request):
