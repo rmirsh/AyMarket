c924813a2a585b44f8ad51e7bba769cd4dae42bc
Kamil Ayupov <ayupovkamil@gmail.com>
2023-11-16 11:25:39 +0300

    Added favicon

--- aymarket/aymarket/settings.py
+++ aymarket/aymarket/settings.py
@@ -120,6 +120,8 @@ USE_TZ = True
 STATIC_URL = 'static/'
 MEDIA_URL = 'media/'
 MEDIA_ROOT = BASE_DIR / 'media'
+STATICFILES_DIRS = [BASE_DIR / "static"]
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
--- aymarket/core/views.py
+++ aymarket/core/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from item.models import Category, Item
 
@@ -6,9 +6,10 @@ from item.models import Category, Item
 def index(request):
     items = Item.objects.filter(is_sold=False)[0:6]
     categories = Category.objects.all()
+
     return render(request, 'core/index.html', {
+        'categories': categories,
         'items': items,
-        'categories': categories
     })
 
 
